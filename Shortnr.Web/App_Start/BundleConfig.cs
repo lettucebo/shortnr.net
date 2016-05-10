d63c72a7af43874a85317a6ccf4cdaf4c0938a52
紙鈔魚 <lettucebo@hotmail.com>
2016-05-10 22:53:19 +0800

    change css framework to Materialize

--- Shortnr.Web/App_Start/BundleConfig.cs
+++ Shortnr.Web/App_Start/BundleConfig.cs
@@ -19,12 +19,10 @@ namespace Shortnr.Web
 			bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
 						"~/Scripts/modernizr-*"));
 
-			bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-					  "~/Scripts/bootstrap.js",
+            bundles.Add(new ScriptBundle("~/bundles/respond").Include(
 					  "~/Scripts/respond.js"));
 
 			bundles.Add(new StyleBundle("~/Content/css").Include(
-					  "~/Content/bootstrap.css",
 					  "~/Content/site.css"));
 		}
 	}
